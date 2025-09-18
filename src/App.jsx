f453a013b167091489fb5e7a6e0a753cb8856b3b
sahulakki123 <lakkisahus04@gmail.com>
2025-09-18 12:36:56 +0530

    project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+function App(){
+
+    // Javascript
+    // let myname = "Lakki sahu"
+    // npm install react-icons --save
+    
+
+    return(
+
+        // React.Fragment
+        <>
+        <Header/>
+        <Footer/>
+        {/* <h1 id="ok">Hello world </h1>
+        <p className="okkk">My Name is {myname}</p> */}
+        </>
+    )
+}
+
+export default App
